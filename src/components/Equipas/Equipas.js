12c29bc12ad7dfa9e8efced2c4964e7bfe930c5b
sbvog <sofiabermudez@ua.pt>
2023-01-06 15:01:09 +0000

    unversioned files

---
+++ src/components/Equipas/Equipas.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Equipas.css";
+
+const Equipas = () => {
+  return (
+    <div className="CompEquipas">
+        <br></br>
+        <h5>
+        Equipas
+        </h5>
+        <br></br>
+        <button id='equipa1'></button>
+        <button id='equipa2'></button>
+        <button id='equipa3'></button>
+    </div>
+  );
+};
+
+export default Equipas;
