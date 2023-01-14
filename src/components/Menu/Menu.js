67ec91461b78ef3015fd17056b87eeadbdc425a2
sbvog <sofiabermudez@ua.pt>
2023-01-14 13:58:54 +0000

    hm

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -16,22 +16,26 @@ const Menu = () => {
         <div className='flex-child-element magenta'>
             <div id='Tracker'>
 
-                <a href='/tracker'><img className="imgMenu" src={require('./Compostor_Tracker_Copy.png')} alt='compostericon'/></a>
+                <a href='/tracker'><img className="imgMenu" src={require('./Compostor_Tracker_Copy.png')}
+                                        alt='compostericon'/></a>
 
             </div>
-            <div>
-                <a href='/learnmore'><img id='LearnMore' className="mt-2 imgMenu" src={require('./icons8-repository-96.png')} alt='Learn more Button'/></a>
-            </div>
+
         </div>
-
         <div className="flex-child-element green">
             <div>
-                <a href='#GoToTurnos'><img id='Turnos' className="imgMenu" src={require('./icons8-calendar-96.png')} alt='turnos'/></a>
+
+                <a href='/learnmore'><img id='LearnMore' className="mt-2 imgMenu"
+                                          src={require('./icons8-repository-96.png')} alt='Learn more Button'/></a>
+            </div>
+        </div>
+        <div className="flex-child-element green">
+            <div>
+                <a href='#GoToTurnos'><img id='Turnos' className="imgMenu" src={require('./icons8-calendar-96.png')}
+                                           alt='turnos'/></a>
 
             </div>
-            <div>
-                <a href='#GoToEquipas'> <img id='Equipas' className="imgMenu" src={require('./icons8-community-96.png')} alt='equipas'/></a>
-            </div>
+
         </div>
     
    
